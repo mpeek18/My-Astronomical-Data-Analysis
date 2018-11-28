9b161db6a01cbe68aed23863c1f4da33d7b34fbf
mpeek18 <33673012+mpeek18@users.noreply.github.com>
2018-11-27 22:20:00 -0500

    Messed something up

--- SDSSQuery/queryPackage/Main.py
+++ SDSSQuery/queryPackage/Main.py
@@ -30,4 +30,4 @@ def switch(longitude, latitude, radiusMultiplier, argv, targetID=None):
         return run.plotMagnitudes(longitude, latitude, radiusMultiplier)
 
 if __name__ == "__main__":
-    switch(143.50993, 55.239775, 12, 3, 98419841984149)    
+    switch(143.50993, 55.239775, 0, 98419841984149)    
